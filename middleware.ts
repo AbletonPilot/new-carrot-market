2799de0cdff949977625f05fb3612411c13b12ce
Logan <sonjm0428@gmail.com>
2024-07-30 19:09:57 +1000

    matcher

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
-import getSession from "./lib/session";
+import { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  // console.log(request.nextUrl);
-  // console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  console.log("hello");
 }
+
+export const config = {
+  // matcher:["/", "/profile", "/create-account", "/user/:path*"],
+  //regular expression
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
